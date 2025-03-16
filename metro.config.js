cd6a6973e1987534246c2f69b837c2ba1dc180db
Mufeel Ishad <99629231+it21159480@users.noreply.github.com>
2025-03-16 12:05:20 +0530

    Add Home and Profile screens with navigation setup; update Babel and Metro configurations

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,44 @@
+// const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
+// /**
+//  * Metro configuration
+//  * https://reactnative.dev/docs/metro
+//  *
+//  * @type {import('@react-native/metro-config').MetroConfig}
+//  */
+// const config = {};
+
+// module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
+// Default Metro configuration
+const defaultConfig = getDefaultConfig(__dirname);
+const {assetExts, sourceExts} = defaultConfig.resolver;
 
 /**
  * Metro configuration
- * https://reactnative.dev/docs/metro
+ * https://facebook.github.io/metro/docs/configuration
  *
- * @type {import('@react-native/metro-config').MetroConfig}
+ * @type {import('metro-config').MetroConfig}
  */
-const config = {};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+const {
+  wrapWithReanimatedMetroConfig,
+} = require('react-native-reanimated/metro-config');
+
+// Custom Metro config
+const customConfig = {
+  transformer: {
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  },
+  resolver: {
+    assetExts: assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...sourceExts, 'svg'],
+  },
+};
+
+// Wrap with Reanimated's Metro config
+module.exports = wrapWithReanimatedMetroConfig(
+  mergeConfig(defaultConfig, customConfig),
+);
